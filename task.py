2e10e50dbb00fb23aba682ff822cd73f4d1d5f21
Lucas Mateus <lmateus1067@outlook.com>
2022-07-06 17:02:46 -0300

    adicionando fubção de salvar binario

--- task.py
+++ task.py
@@ -2,16 +2,23 @@ import calendar
 import os
 import datetime
 import pickle  # biblioteca pra gravar o dicionario inteiro no arquivo
-# usar a função dump para passar tudo de um dicionario para o arquivo de uma vez só
+
 os.system('cls')  # clear so funcionar no linux, pra windows o comando é cls
+
+#dicionario com os usuarios, vamos usar os integrantes do grupo como padrão
+
 us = {
 
+    "Lucas" : ["1234"],
+    "Vinicius" : ["4321"]
+
 }
-l = ['as', 'adsad', 'fafasfsa']
-arq = open('users', 'wt')  # criando arquivo de usuários
-for i in l:
-    arq.write(i + '\n')  # pasando da lista para o arquivo
+
+arq = open('users.dat', 'wb')  # criando arquivo de usuários
+pickle.dump(us, arq) # usar a função dump para passar tudo de um dicionario para o arquivo de uma vez só
 arq.close()
+
+
 # assim o programa funcionara pra qualquer ano, então é bom trabalhar sempre com datas variaveis
 anomenu = datetime.datetime.now().year
 
@@ -31,29 +38,55 @@ esc1 = input('Escolha sua opção: ')
 while esc1 != "0":
 
     if esc1 == "1":
-        print("=== Seleção de usuário ===")
+        print("=== Seleção de usuário ===\n")
+        try:
+            arq = open("users.dat", "rb")
+            us = pickle.load(arq)
+            arq.close()
+        except: 
+            arq = open("users.dat", "wb")
+            arq.close()
+
+        for users in us:
+            print(users)
+
         print("=== Em Desenvolvimento ===")
+
     elif esc1 == "2":
+
         print("=== Cadastro de usuário ===")
         nome = input('Insira seu nome: ')
         senha = input('Insira sua senha:')
+
         if us.get(nome):
+
             print('Usuário já existe', nome)
         else:
+
             us[nome] = senha
+            arq = open('users.dat', 'wb')
+            pickle.dump(us, arq) 
+            arq.close()
+
         print("=== Em Desenvolvimento ===")
     elif esc1 == "3":
+
         print("===  Atualizar usuário   =")
         nome = input('Insira o nome usuário a se mudar:')
         if nome in us.keys():
+
             nome2 = input('Insira o novo nome: ')
             senha = input('Insira a nova senha: ')
             us[nome2] = senha
             del us[nome]
         else:
+
             print('Nome não encontrado!')
+
         print("=== Em Desenvolvimento ===")
+
     elif esc1 == "4":
+
         print("===   Função Deletar   ===")
         nome = input('Insira o usuário que será deletado:')
         if nome in us.keys():

06ce38a47d3407e346a9b2be50e515ea5d5fc829
Lucas Mateus <lmateus1067@outlook.com>
2022-07-18 13:49:23 -0300

    add func inserir data

--- validardata.py
+++ validardata.py
@@ -1,3 +1,4 @@
+from curses.ascii import isdigit
 import datetime
 import os
 
@@ -9,56 +10,65 @@ def inserirdata():  # função de inserir data
 
     while not(valida):
 
-        dia = int(input("Por favor insira o dia: "))
-        mes = int(input("Por favor insira o mês: "))
-        ano = int(input("Por favor insira o ano: "))
+        dias = input("Por favor insira o dia: "))
+        mess = (input("Por favor insira o mês: "))
+        anos = (input("Por favor insira o ano: "))
 
-        anoc = ano % 4
-        anob = ano % 100
-        anod = ano % 400
-
-        if mes > 12 or ano == 0 or mes == 0 or dia == 0:
-
+        if (dias.isdigit() and mess.isdigit() and anos.isdigit()) != True:
             print("Data invalida")
-
+            print("Insira somente numeros")
         else:
-            if mes == 2:
 
-                if (anoc == 0) and (anob != 0) or (anod != 0):
+            dia = int(dias)
+            mes = int(mess)
+            ano = int(anos)
+            
+            anoc = ano % 4
+            anob = ano % 100
+            anod = ano % 400
 
-                    bi = 0
+            if mes > 12 or ano == 0 or mes == 0 or dia == 0:
 
-                elif (anoc == 0) and (anob != 0) or (anod == 0):
-
-                    bi = 1
-
-            elif (dia >= 32) and ((mes == 1) or (mes == 3) or (mes == 5) or (mes == 7) or (mes == 8) or (mes == 10) or (mes == 12)):
-
-                print("seu dia inserido é invalido pois seu mês so tem 31 dias")
-
-            elif (dia >= 31) and ((mes == 4) or (mes == 6) or (mes == 9) or (mes == 11)):
-
-                print("seu dia inserido é invalido pois seu mês so tem 30 dias")
-
-            elif (dia == 29) and (mes == 2) and (bi == 0):
-
-                print("seu dia inserido é invalido pois seu mês so tem 28 dias")
-
-            elif (dia == 30) and (mes == 2) and (bi == 1):
-
-                print(
-                    "seu dia inserido é invalido pois apesar de ser ano bissexto fevereiro so tem 29 dias")
+                print("Data invalida")
 
             else:
+                if mes == 2:
 
-                vld = evalido(dia, mes, ano)
+                    if (anoc == 0) and (anob != 0) or (anod != 0):
 
-                if vld == True:
+                        bi = 0
 
-                    print("Data valida")
-                    datavalida = f"{dia:0>2}/{mes:0>2}/{ano}"
-                    print(datavalida)
-                    valida = True
+                    elif (anoc == 0) and (anob != 0) or (anod == 0):
+
+                        bi = 1
+
+                elif (dia >= 32) and ((mes == 1) or (mes == 3) or (mes == 5) or (mes == 7) or (mes == 8) or (mes == 10) or (mes == 12)):
+
+                    print("seu dia inserido é invalido pois seu mês so tem 31 dias")
+
+                elif (dia >= 31) and ((mes == 4) or (mes == 6) or (mes == 9) or (mes == 11)):
+
+                    print("seu dia inserido é invalido pois seu mês so tem 30 dias")
+
+                elif (dia == 29) and (mes == 2) and (bi == 0):
+
+                    print("seu dia inserido é invalido pois seu mês so tem 28 dias")
+
+                elif (dia == 30) and (mes == 2) and (bi == 1):
+
+                    print(
+                        "seu dia inserido é invalido pois apesar de ser ano bissexto fevereiro so tem 29 dias")
+
+                else:
+
+                    vld = evalido(dia, mes, ano)
+
+                    if vld == True:
+
+                        print("Data valida")
+                        datavalida = f"{dia:0>2}/{mes:0>2}/{ano}"
+                        print(datavalida)
+                        valida = True
 
     return datavalida
 
